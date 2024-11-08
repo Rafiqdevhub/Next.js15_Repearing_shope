d00919b2f8d9c9df4d83f25d3b57c5670da1a479
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-11-08 20:30:22 +0500

    Refactor Sentry configuration in next.config.ts to simplify setup and update project details

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,17 @@
 /** @type {import('next').NextConfig} */
 import { withSentryConfig } from "@sentry/nextjs";
 
-const nextConfig = {
-  experimental: {
-    instrumentationHook: true,
-  },
-};
+const nextConfig = {};
 
+// Make sure adding Sentry options is the last code to run before exporting
 export default withSentryConfig(nextConfig, {
-  org: process.env.SENTRY_ORG,
-  project: process.env.NEXT_SENTRY_PROJECT,
+  org: "davegray",
+  project: "repairshop",
 
+  // An auth token is required for uploading source maps.
   authToken: process.env.SENTRY_AUTH_TOKEN,
 
-  silent: false,
+  silent: false, // Can be used to suppress logs
 
   hideSourceMaps: true,
 
