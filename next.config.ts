ef3d683e540dd4a2c21b75475953c04f31a39957
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-11-08 20:33:42 +0500

    Refactor Sentry configuration in next.config.ts to streamline options and enhance clarity

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,13 @@ import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig = {};
 
-// Make sure adding Sentry options is the last code to run before exporting
 export default withSentryConfig(nextConfig, {
   org: "davegray",
   project: "repairshop",
 
-  // An auth token is required for uploading source maps.
   authToken: process.env.SENTRY_AUTH_TOKEN,
 
-  silent: false, // Can be used to suppress logs
-
+  silent: false,
   hideSourceMaps: true,
 
   disableLogger: true,
