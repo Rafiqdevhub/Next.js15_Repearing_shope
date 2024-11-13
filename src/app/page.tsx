f6b9c72043a98bf93e3529fc0842f11a522f63e9
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-11-13 05:42:03 +0500

    Update homepage title and address for branding consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,14 @@ const Home = () => {
       <main className="flex flex-col justify-center text-center max-w-5xl mx-auto h-dvh">
         <div className="flex flex-col gap-6 p-12 rounded-xl bg-black/90 w-4/5 sm:max-w-96 mx-auto text-white sm:text-2xl">
           <h1 className="text-4xl font-bold">
-            Repairify&apos;s Computer
+            Repairify&apos;s
             <br />
             Repair Shop
           </h1>
           <address>
-            gul khan shope
+            Repairify shope
             <br />
-            Peshawer City, KS 55555
+            Peshawer City
           </address>
           <p>Open Daily: 9am to 5pm</p>
           <Link href="tel:92333333" className="hover:underline">
