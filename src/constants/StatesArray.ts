df46ad40bffa3852a45b308f2fa261c1c49f218c
Muhammad Rafiq <rafkhan9323@gmail.com>
2024-11-20 20:38:13 +0500

    form added for both customers and tickets

---
+++ src/constants/StatesArray.ts
@@ -0,0 +1,9 @@
+export const StatesArray = [
+  { id: "IS", description: "Islamabad" },
+  { id: "PS", description: "Peshawar" },
+  { id: "B", description: "Board" },
+  { id: "Hashnaghari", description: "HH" },
+  { id: "QK", description: "Qisakhwani" },
+  { id: "KB", description: "Khyber Bazar" },
+  { id: "CT", description: "Islamabad City" },
+];
